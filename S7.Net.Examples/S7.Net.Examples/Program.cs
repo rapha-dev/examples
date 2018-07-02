bdef2926c77b7ccda57048dfd12e46f54e60e9a9
Raphael <rs@sltgmbh.com>
2018-07-02 11:19:09 +0200

    Example for reading a single Bit via ReadClass

---
+++ S7.Net.Examples/S7.Net.Examples/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S7.Net.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var ip = "10.10.11.38";
+            var plc = new S7.Net.Plc(CpuType.S7300, ip, 0, 2);
+
+            Console.WriteLine("Connecting " + ip);
+            plc.Open();
+
+            Console.WriteLine("Reading items");
+            var tc = new ByteTest();
+            plc.ReadClass(tc, 800, 0);
+
+            plc.Close();
+
+            // result
+            Console.WriteLine("Results: ");
+            var t = typeof(ByteTest);
+            PropertyInfo[] propInfos = t.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (var item in propInfos)
+            {
+                var val = tc.GetType().GetProperty(item.Name).GetValue(tc, null);
+                Console.WriteLine("{0}: {1}", item.Name, val.ToString());
+            }
+
+            // keep console open
+            Console.WriteLine("Press [Enter] to exit.");
+            Console.Read();
+        }
+    }
+}
