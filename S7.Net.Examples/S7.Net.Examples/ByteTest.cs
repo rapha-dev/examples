0f83ce6a261995d8efaa48a8315b33e11d73af85
Raphael <rs@sltgmbh.com>
2018-07-02 11:26:10 +0200

    Expanded to 10 Bits

--- S7.Net.Examples/S7.Net.Examples/ByteTest.cs
+++ S7.Net.Examples/S7.Net.Examples/ByteTest.cs
@@ -9,13 +9,21 @@ namespace S7.Net.Examples
     public class ByteTest
     {
         public bool Bit0 { get; set; }
-        //public bool Bit1 { get; set; }
-        //public bool Bit2 { get; set; }
-        //public bool Bit3 { get; set; }
-        //public bool Bit4 { get; set; }
-        //public bool Bit5 { get; set; }
-        //public bool Bit6 { get; set; }
-        //public bool Bit7 { get; set; }
+        public bool Bit1 { get; set; }
+        public bool Bit2 { get; set; }
+        public bool Bit3 { get; set; }
+        public bool Bit4 { get; set; }
+        public bool Bit5 { get; set; }
+        public bool Bit6 { get; set; }
+        public bool Bit7 { get; set; }
 
+        public bool Bit8 { get; set; }
+        public bool Bit9 { get; set; }
+        //public bool Bit10 { get; set; }
+        //public bool Bit11 { get; set; }
+        //public bool Bit12 { get; set; }
+        //public bool Bit13 { get; set; }
+        //public bool Bit14 { get; set; }
+        //public bool Bit15 { get; set; }
     }
 }
